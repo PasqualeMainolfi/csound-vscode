e1380cead4a180ae7170127f2ded7bd5e69b198f
PasqualeMainolfi <mnlpql@gmail.com>
2025-12-16 10:36:00 +0100

    before push

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Change Log
+
+## [Unreleased]
+
+- Initial release
+
+## [0.0.1] - 2025-15-12
+
+### Added
+
+### Fixed
+
+### Changed
+
+### Removed
---
+++ src/extension.ts
@@ -0,0 +1,86 @@
+import * as vscode from 'vscode';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
+
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions
+} from 'vscode-languageclient/node';
+
+let client: LanguageClient;
+
+export async function activate(context: vscode.ExtensionContext) {
+  const serverPath = getServerPath(context);
+
+  if (!serverPath) { return; }
+
+  const serverOptions: ServerOptions = {
+    command: serverPath,
+    args: [],
+  };
+
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [
+      { scheme: "file", language: "csound" }
+    ],
+  };
+
+  client = new LanguageClient(
+    "csound-lsp",
+    "CSound Language Server",
+    serverOptions,
+    clientOptions
+  );
+
+  client.start();
+}
+
+export function deactivate() {
+  return client?.stop();
+}
+
+function getServerPath(context: vscode.ExtensionContext): string | undefined {
+  const binDir = path.join(context.extensionPath, 'bin');
+  
+  const platform = os.platform();
+  const arch = os.arch();
+
+  let binaryName = '';
+
+  if (platform === 'win32') {
+    binaryName = 'csound-lsp-windows.exe';
+  } 
+  else if (platform === 'linux') {
+    binaryName = 'csound-lsp-linux';
+  } 
+  else if (platform === 'darwin') {
+    if (arch === 'arm64') {
+      binaryName = 'csound-lsp-macos-arm64';
+    } else {
+      binaryName = 'csound-lsp-macos-x64';
+    }
+  } 
+  else {
+    vscode.window.showErrorMessage(`OS not supported: ${platform}`);
+    return undefined;
+  }
+
+  const fullPath = path.join(binDir, binaryName);
+
+  if (!fs.existsSync(fullPath)) {
+    vscode.window.showErrorMessage(`LSP not founded in: ${fullPath}`);
+    return undefined;
+  }
+
+  if (platform !== 'win32') {
+    try {
+        fs.chmodSync(fullPath, '755');
+    } catch (e) {
+        console.warn("permission denied +x:", e);
+    }
+  }
+
+  return fullPath;
+}
